845f13c0891b96733872070eca72b3bd505f76b9
inesoraya <e41230379@student.polije.ac.id>
2024-02-05 21:54:01 +0700

    Update pertama.java

--- pertama.java
+++ pertama.java
@@ -1,1 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package acara6;
+import java.util.Scanner;
+public class pertama {
+   public static void main(String[] args) {
+       int nilai;
+       String grade;
+       Scanner scan = new Scanner (System.in);
+       System.out.print("Inputkan nilai: ");
+       nilai = scan.nextInt();
+       
+       if (nilai >= 90){
+           grade = "A";
+       } else if (nilai >= 80) {
+           grade = "B+";
+       } else if (nilai >= 70) {
+           grade = "B";
+       } else if (nilai >= 60) {
+           grade = "C+";
+       } else if (nilai >= 50) {
+           grade = "C";
+       } else if (nilai >= 40) {
+           grade = "D";
+       } else {
+           grade = "E";
+       }
+       System.out.println("Grade: " + grade);
+   } 
+}
